fc9e28872f074bad29ce361be3f89865be0a0a59
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-11-02 09:15:09 +0100

    added master-route in app.js to prevent conflicts

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var notemaster = require('./routes/note-master');
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/note-master', notemaster);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -37,6 +39,7 @@ app.use(function (req, res, next) {
     err.status = 404;
     next(err);
 });
+
 
 // error handler
 app.use(function (err, req, res, next) {
