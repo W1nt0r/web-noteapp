fafa912d06868b8b882dc704f6b0c2c65d9834bd
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-11-08 13:53:50 +0100

    style of selected button fixed

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,20 +1,21 @@
 const noteModel = require("../service/note.js");
 
-module.exports.showIndex = function(req, res) {
+module.exports.showIndex = function (req, res) {
     let sort = getSortCriteria(req);
+    let sortStates = getSortStates(req);
     let nextTheme = getNextTheme(req);
-    console.log(sort);
     let filter = {};
     let isFiltered = isFilter(req);
-    if(isFiltered) {
+    if (isFiltered) {
         filter["done"] = false;
     }
 
+
     noteModel.all(filter, sort, (err, notes) => {
-        if(err) {
+        if (err) {
             //show/render errors
         } else {
-            res.render("note-master", {notes: notes, filterState: !isFiltered, style: nextTheme});
+            res.render("note-master", {notes: notes, filterState: !isFiltered, style: nextTheme, sortStates: sortStates});
         }
     });
 
@@ -26,7 +27,7 @@ module.exports.showNewNote = function (req, res) {
 
 module.exports.showEditNote = function (req, res) {
     noteModel.get(req.params.id, (err, note) => {
-        if(err) {
+        if (err) {
             //show/render errors
         } else {
             res.render("note-detail", {pagetitle: "Ändere Todo", note: note});
@@ -34,10 +35,10 @@ module.exports.showEditNote = function (req, res) {
     });
 };
 
-module.exports.createNote = function(req, res) {
+module.exports.createNote = function (req, res) {
     noteChangeHelper(req, res, (title, description, importance, dueDate, done) => {
         noteModel.add(title, description, importance, dueDate, done, (err, newNotes) => {
-            if(err) {
+            if (err) {
                 //show/render errors
             } else {
                 res.redirect("/");
@@ -49,7 +50,7 @@ module.exports.createNote = function(req, res) {
 module.exports.updateNote = function (req, res) {
     noteChangeHelper(req, res, (title, description, importance, dueDate, done, id) => {
         noteModel.update(id, title, description, importance, dueDate, done, (err, note) => {
-            if(err) {
+            if (err) {
                 //show/render errors
             } else {
                 console.log(note.dueDate);
@@ -63,7 +64,7 @@ module.exports.updateNote = function (req, res) {
 function noteChangeHelper(req, res, callback) {
     let errMsg = validateNoteFields(req.body);
 
-    if(errMsg.length == 0) {
+    if (errMsg.length == 0) {
         let title = req.body.title;
         let description = req.body.description;
         let importance = Number(req.body.importance);
@@ -72,11 +73,11 @@ function noteChangeHelper(req, res, callback) {
         let done = false;
         let id = null;
 
-        if(req.body.done && req.body.done == "done") {
+        if (req.body.done && req.body.done == "done") {
             done = true;
         }
 
-        if(req.params.id) {
+        if (req.params.id) {
             id = req.params.id;
         }
 
@@ -87,15 +88,15 @@ function noteChangeHelper(req, res, callback) {
 }
 
 function getNextTheme(req) {
-    if(req.query.style) {
+    if (req.query.style) {
         req.session.theme = req.query.style;
 
-        if(req.query.style == "dark") {
+        if (req.query.style == "dark") {
             return "light";
         }
     }
 
-    if(req.session.theme == "dark") {
+    if (req.session.theme == "dark") {
         return "light";
     } else {
         return "dark";
@@ -103,8 +104,8 @@ function getNextTheme(req) {
 }
 
 function isFilter(req) {
-    if(req.query.filter) {
-        if(req.query.filter.length != 0) {
+    if (req.query.filter) {
+        if (req.query.filter.length != 0) {
             let value = (req.query.filter == "true");
             req.session.filter = value;
 
@@ -112,7 +113,7 @@ function isFilter(req) {
         }
     }
 
-    if(req.session.filter) {
+    if (req.session.filter) {
         return req.session.filter;
     }
 
@@ -123,7 +124,7 @@ function getSortCriteria(req) {
     let sort = {};
     prepareSortCriterias(req);
 
-    if(req.session.sort.length != 0) {
+    if (req.session.sort.length != 0) {
         let fieldName = req.session.sort[0];
         let ascDesc = req.session.sort[1];
 
@@ -133,21 +134,34 @@ function getSortCriteria(req) {
     return sort;
 }
 
+function getSortStates(req) {
+    let sortStates = {
+        dueDate: false,
+        creationDate: false,
+        importance: false
+    };
+    if (!req.session.sort) {
+        return sortStates;
+    }
+    sortStates[req.session.sort[0]] = true;
+    return sortStates;
+}
+
 function prepareSortCriterias(req) {
-    if(req.query.sort) {
+    if (req.query.sort) {
         let sort = req.query.sort;
 
-        if(!req.session.sort) {
+        if (!req.session.sort) {
             req.session.sort = [sort, 1];
         } else {
-            if(req.session.sort[0] == sort) {
+            if (req.session.sort[0] == sort) {
                 req.session.sort[1] = -req.session.sort[1];
             } else {
                 req.session.sort = [sort, 1];
             }
         }
     } else {
-        if(!req.session.sort) {
+        if (!req.session.sort) {
             req.session.sort = [];
         }
     }
@@ -156,34 +170,34 @@ function prepareSortCriterias(req) {
 function validateNoteFields(params) {
     let errMsg = [];
 
-    if(!params.title || params.title == "") {
+    if (!params.title || params.title == "") {
         errMsg.push("Title is not set");
     }
 
-    if(!params.description || params.description == "") {
+    if (!params.description || params.description == "") {
         errMsg.push("Description is not set");
     }
 
-    if(!params.importance || params.importance == "") {
+    if (!params.importance || params.importance == "") {
         errMsg.push("Importance is not set");
     } else {
         let importance = Number(params.importance);
-        
-        if(isNaN(importance)) {
+
+        if (isNaN(importance)) {
             errMsg.push("Importance is not a number");
         } else {
-            if(importance < 1 || importance > 5) {
+            if (importance < 1 || importance > 5) {
                 errMsg.push("Importance is out of bound [1;5]: " + importance);
             }
         }
     }
 
-    if(!params.dueDate || params.dueDate == "") {
+    if (!params.dueDate || params.dueDate == "") {
         errMsg.push("Due Date is not set");
     } else {
         let date = new Date(params.dueDate);
 
-        if(!(date instanceof Date) || isNaN(date.valueOf())) {
+        if (!(date instanceof Date) || isNaN(date.valueOf())) {
             errMsg.push("Due Date is not a date");
         }
     }
